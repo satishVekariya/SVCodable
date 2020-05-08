b63858780959e987163e8c6953f64d19eadcd85f
satish <satish.v@canopas.com>
2020-05-08 10:16:58 +0530

    added codable extension

--- Sources/SVCodable/SVCodable.swift
+++ Sources/SVCodable/SVCodable.swift
@@ -1,3 +1,46 @@
-struct SVCodable {
-    var text = "Hello, World!"
+import Foundation
+
+extension Encodable {
+    func toJSONData() -> Data? {
+        return try? JSONEncoder().encode(self)
+    }
+    
+    func toJSONString() -> String? {
+        if let bytes = toJSONData() {
+            return String(bytes: bytes, encoding: .utf8)
+        }
+        return nil
+    }
+    
+    func toJSON() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self) else { return [:] }
+        return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] } ?? [:]
+    }
+}
+
+extension Decodable {
+    static func map(JSONString:String) -> Self? {
+        do {
+            return try JSONDecoder().decode(Self.self, from: Data(JSONString.utf8))
+        } catch let error {
+            print(error)
+            return nil
+        }
+    }
+    
+    static func map(JSONObject: Any) -> Self? {
+        guard JSONSerialization.isValidJSONObject(JSONObject) else {
+            print("JSONSerialization : Invalid object")
+            return nil
+        }
+        if let data = try? JSONSerialization.data(withJSONObject: JSONObject, options: .prettyPrinted) {
+            do {
+                return try JSONDecoder().decode(Self.self, from: data)
+            } catch let error {
+                print(error.localizedDescription)
+                return nil
+            }
+        }
+        return nil
+    }
 }
--- Tests/SVCodableTests/SVCodableTests.swift
+++ Tests/SVCodableTests/SVCodableTests.swift
@@ -6,7 +6,7 @@ final class SVCodableTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SVCodable().text, "Hello, World!")
+//        XCTAssertEqual(SVCodable().text, "Hello, World!")
     }
 
     static var allTests = [
